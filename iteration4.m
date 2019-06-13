ee54e1bcfaf620c685ffc66d3e57304cbaf2ba5f
Brandon Chen <datbrandontran@gmail.com>
2019-06-13 11:24:14 -0700

    Update iteration4

--- iteration4.m
+++ iteration4.m
@@ -91,18 +91,9 @@ class_4_ur = Ur(index_class_4_ur(1,1):index_class_5_ur(1,1) - 1,:);
 class_5_ur = Ur(index_class_5_ur(1,1):index_class_6_ur(1,1) - 1,:);
 class_6_ur = Ur(index_class_6_ur(1,1):end,:);
 
-% % Reduce the size of the classes to be consistent across all 4 with
-% % respects to the smallest class
-% [r1, c1] = size(class_1);
-% class_1_ur = class_1_ur(1:r1,2:end);
-% class_2_ur = class_2_ur(1:r1,2:end);
-% class_3_ur = class_3_ur(1:r1,2:end);
-% class_4_ur = class_4_ur(1:r1,2:end);
-% 
-% Ur = Ur(:,2:end);
+%% Select random sample for transformed data by class
+sample_size = 100;
 
-
-%% Select random sample from classes
 % Class 1
 %get the dimensions of your array   
 [s1, f1] = size(class_1_ur); 
@@ -112,7 +103,7 @@ for i=1:s1
 end 
 % Randomize by sorting the matrix based on the new random column 
 class_1_ur_s1=sortrows(class_1_ur,f1+1); 
-class_1_new = [class_1_ur_s1(1:500,1:f1)];
+class_1_new = [class_1_ur_s1(1:sample_size,1:f1)];
 
 % Class 2
 %get the dimensions of your array   
@@ -123,7 +114,7 @@ for i=1:s2
 end 
 % Randomize by sorting the matrix based on the new random column 
 class_2_ur_s2=sortrows(class_2_ur,f2+1); 
-class_2_new = [class_2_ur_s2(1:500,1:f2)];
+class_2_new = [class_2_ur_s2(1:sample_size,1:f2)];
 
 % Class 3
 %get the dimensions of your array   
@@ -134,7 +125,7 @@ for i=1:s3
 end 
 % Randomize by sorting the matrix based on the new random column 
 class_3_ur_s3=sortrows(class_3_ur,f3+1); 
-class_3_new = [class_3_ur_s3(1:500,1:f3)];
+class_3_new = [class_3_ur_s3(1:sample_size,1:f3)];
 
 % Class 4
 %get the dimensions of your array   
@@ -145,7 +136,7 @@ for i=1:s4
 end 
 % Randomize by sorting the matrix based on the new random column 
 class_4_ur_s4=sortrows(class_4_ur,f4+1); 
-class_4_new = [class_4_ur_s4(1:500,1:f4)];
+class_4_new = [class_4_ur_s4(1:sample_size,1:f4)];
 
 % Class 5
 %get the dimensions of your array   
@@ -156,7 +147,7 @@ for i=1:s5
 end 
 % Randomize by sorting the matrix based on the new random column 
 class_5_ur_s5=sortrows(class_5_ur,f5+1); 
-class_5_new = [class_5_ur_s5(1:500,1:f5)];
+class_5_new = [class_5_ur_s5(1:sample_size,1:f5)];
 
 % Class 6
 %get the dimensions of your array   
@@ -167,6 +158,22 @@ for i=1:s6
 end 
 % Randomize by sorting the matrix based on the new random column 
 class_6_ur_s6=sortrows(class_6_ur,f6+1); 
-class_6_new = [class_6_ur_s6(1:500,1:f6)];
+class_6_new = [class_6_ur_s6(1:sample_size,1:f6)];
 
+%% 2D Scatter Plots of Transformed Features
+[r1, c1] = size(class_1_new); 
+
+% Acoustic, dance
+subplot(6,6,1)
+hold on
+scatter(class_1_new(:,2), class_1_new(:,3),'r', '.')
+scatter(class_2_new(:,2), class_2_new(:,3),'b', '.')
+scatter(class_3_new(:,2), class_3_new(:,3),'g', '.')
+scatter(class_4_new(:,2), class_4_new(:,3),'k', '.')
+scatter(class_5_new(:,2), class_5_new(:,3),'m', '.')
+scatter(class_6_new(:,2), class_6_new(:,3),'c', '.')
+title('Sepal Length vs. Sepal Width')
+xlabel('Sepal Length (cms)')
+ylabel('Sepal Width (cms)')
+hold off
 
